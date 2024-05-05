5b4a8e8c0e96aabf21e85be2c83e23cd22981979
Marvid21.Marcelo <Marvid21.arguellomarcelo48@gmail.com>
2024-05-04 22:37:29 -0400

    se modifican main, y plugins requeridos

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pokedex_marce/controllers/pokemon_basic_controller.dart';
+import 'package:pokedex_marce/controllers/pokemon_more_info_controller.dart';
+import 'package:pokedex_marce/controllers/pokemon_stats_controller.dart';
+import 'package:pokedex_marce/ui/screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PokemonBasicDataController()),
+        ChangeNotifierProvider(create: (_)=> PokemonMoreInfoController()),
+        ChangeNotifierProvider(create: (_) => PokemonStatsController()),
+      ],
+      child: Consumer (
+        builder: (context, provider, ch) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Pokedex',
+            initialRoute: HomeScreen.routname,
+            routes: {
+              HomeScreen.routname:(context) => const HomeScreen(),
+            },
+          );
+        },
       ),
     );
   }
